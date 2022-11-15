1a445dd4d01e8b888826691fc97dd51c34975a17
Neil <barrymun@tcd.ie>
2022-11-15 10:06:21 +0000

    working play/pause

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
+let audio: HTMLAudioElement = new Audio('/public/theme.mp3');
+
+const playBtn: HTMLElement | null = document.getElementById('play');
+playBtn!.onclick = () => {
+  const isPlaying: boolean = document.body.dataset.playing === "true";
+
+  // play/pause the music
+  if (isPlaying) audio.pause();
+  else audio.play();
+  // change the display text
+  playBtn!.innerText = isPlaying ? "Play" : "Pause";
+  // toggle the playing data var
+  document.body.dataset.playing = isPlaying ? "false" : "true";
+};
+
 export {}
