45d8557675d80a37883d5e424b2f7e7c4f0b5aae
Neil <barrymun@tcd.ie>
2022-11-15 10:52:05 +0000

    instantiating context seems to break the flow when page is reloaded (music does not play subsequently)

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,35 @@
-let audio: HTMLAudioElement | null = document.querySelector('audio');
+(() => {
+  // const AudioContext = window.AudioContext;
+  // const audioCtx: AudioContext = new AudioContext();
+  const audio: HTMLMediaElement | null = document.querySelector('audio');
+  console.log(audio)
+  // if (audio == null) return;
+  // const track: MediaElementAudioSourceNode = audioCtx.createMediaElementSource(audio!);
+  // const analyser = audioCtx.createAnalyser();
+  // track.connect(analyser)
+  // analyser.connect(audioCtx.destination);
+  // console.log(analyser.frequencyBinCount)
 
-const playBtn: HTMLElement | null = document.getElementById('play');
-playBtn!.onclick = () => {
-  const isPlaying: boolean = document.body.dataset.playing === "true";
+  // const gainNode: GainNode = audioCtx.createGain();
+  //
+  // // panning
+  // const pannerOptions = {pan: 0};
+  // const panner = new StereoPannerNode(audioCtx, pannerOptions);
+  //
+  // track.connect(gainNode).connect(panner).connect(audioCtx.destination);
 
-  // play/pause the music
-  if (isPlaying) audio?.pause();
-  else audio?.play();
-  // change the display text
-  playBtn!.innerText = isPlaying ? "Play" : "Pause";
-  // toggle the playing data var
-  document.body.dataset.playing = isPlaying ? "false" : "true";
-};
+  const playBtn: HTMLElement | null = document.getElementById('play');
+  playBtn!.addEventListener("click", () => {
+    const isPlaying: boolean = document.body.dataset.playing === "true";
+
+    // play/pause the music
+    if (isPlaying) audio!.pause();
+    else audio!.play();
+    // change the display text
+    playBtn!.innerText = isPlaying ? "Play" : "Pause";
+    // toggle the playing data var
+    document.body.dataset.playing = isPlaying ? "false" : "true";
+  });
+})()
 
 export {}
