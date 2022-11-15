4ce72aac35741e2c3002d67629ce08c299786955
Neil <barrymun@tcd.ie>
2022-11-15 10:15:55 +0000

    using audio element

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-let audio: HTMLAudioElement = new Audio('/theme.mp3');
+let audio: HTMLAudioElement | null = document.querySelector('audio');
 
 const playBtn: HTMLElement | null = document.getElementById('play');
 playBtn!.onclick = () => {
   const isPlaying: boolean = document.body.dataset.playing === "true";
 
   // play/pause the music
-  if (isPlaying) audio.pause();
-  else audio.play();
+  if (isPlaying) audio?.pause();
+  else audio?.play();
   // change the display text
   playBtn!.innerText = isPlaying ? "Play" : "Pause";
   // toggle the playing data var
