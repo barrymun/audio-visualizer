0a1fbaac18747696c8eaccea29ed170f685a4052
Neil <barrymun@tcd.ie>
2022-11-15 17:18:13 +0000

    improved val size algorithm

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 (() => {
+  const MAX_VALUE: number = 255;
+
   const audio: HTMLMediaElement = (document.getElementById('audio') as HTMLMediaElement)!;
   const visualizer: HTMLDivElement = (document.getElementById('visualizer') as HTMLDivElement)!;
   const audioCtx = new AudioContext();
   const audioSrc: MediaElementAudioSourceNode = audioCtx.createMediaElementSource(audio!);
   const analyser: AnalyserNode = audioCtx.createAnalyser();
-  // analyser.fftSize = 64;
-  analyser.fftSize = 128;
+  /**
+   * w.r.t. analyser.fftSize:
+   * "A higher value will result in more details in the frequency domain but fewer details in the time domain."
+   */
+  analyser.fftSize = 64;
+  // analyser.fftSize = 128;
+  // analyser.fftSize = 256;
   analyser.smoothingTimeConstant = 0.85;
 
   // connect the source back up to the destination, otherwise the sound won't play
@@ -13,7 +20,7 @@
   audioSrc.connect(audioCtx.destination);
 
   const bufferLength: number = analyser.frequencyBinCount;
-  const dataArray: Uint8Array = new Uint8Array(bufferLength);
+  let dataArray: Uint8Array = new Uint8Array(bufferLength);
 
   let spanElements: Array<HTMLSpanElement> = [];
   for (let i: number = 0; i < bufferLength; i++) {
@@ -23,20 +30,20 @@
     visualizer.appendChild(spanElement);
   }
 
-  const clamp = (num: number, min: number, max: number): number => {
-    if (num >= max) return max;
-    if (num <= min) return min;
-    return num;
-  }
-
+  /**
+   * based on https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/getByteFrequencyData
+   * note that "The frequency data is composed of integers on a scale from 0 to 255." - as per the docs
+   */
   const animate = (): void => {
     requestAnimationFrame(animate);
     analyser.getByteFrequencyData(dataArray);
     for (let i: number = 0; i < bufferLength; i++) {
-      let item = dataArray[i];
-      item = item > 150 ? item / 1.5 : item * 1.5;
-      spanElements[i].style.transform = `rotateZ(${i * (360 / bufferLength)}deg) translate(-50%, ${clamp(item, 100, 150)}px)`;
-      // spanElements[i].style.transform = `rotateZ(${i * (360 / bufferLength)}deg) translate(-50%, 100px)`;
+      let val = dataArray[i];
+      if (val < (MAX_VALUE / 4)) val *= 2;
+      else if (val < (MAX_VALUE / 2)) val *= 1.5;
+      else if (val < (MAX_VALUE / 4) * 3) val /= 1.5;
+      else val /= 2;
+      spanElements[i].style.transform = `rotateZ(${i * (360 / bufferLength)}deg) translate(-50%, ${val * 0.05}rem)`;
     }
   };
   animate();
